98d4b180cb50a52944fcf37fd402b93603d4e2fc
al-mahfuz <almahfuzhossen@gmail.com>
2023-01-23 19:04:05 +0600

    Js file added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-const wrapper = document.querySelector(".wrapper")
+const wrapper = document.querySelector(".wrapper"),
+searchInput = wrapper.querySelector("input"),
+infoText = wrapper.querySelector(".info-text");
+
+// fetch api function
+function fetchApi(word){
+    infoText.innerHTML = `Can't find the meaning of <span>"${word}"</span>. Please, try to search for another word.`;
+}
+
+searchInput.addEventListener("keyup", e =>{
+    if(e.key === "Enter" && e.target.value){
+        fetchApi(e.target.value);
+    }
+});
